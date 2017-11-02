320c81cea6467a37120bb20008d79310505fc6b5
汤龙萍 <tanglongping@99bicycle.com>
2017-11-02 14:06:00 +0800

    inmobi bug修复

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8091,
+    port: 8094,
     host: '0.0.0.0',
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
