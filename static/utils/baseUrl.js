1b107b9eeb9bc15a56484ed0b32d06c33e022b63
tang <tang@qq.com>
2017-12-03 12:57:31 +0800

    merge

--- static/utils/baseUrl.js
+++ static/utils/baseUrl.js
@@ -1,16 +1,10 @@
 let baseUrl
 
-<<<<<<< HEAD
-if (process.env.NODE_ENV === 'PRODUCTION') {
-  baseUrl = 'http://hk.99bicycle.com'
-} else if (process.env.NODE_ENV === 'TEST') {
-=======
 // if (process.env === 'PRODUCTION') {
 if (process.env === 'production') {
   baseUrl = 'http://hk.99bicycle.com'
 // } else if (process.env === 'TEST') {
 } else if (process.env === 'test') {
->>>>>>> b24830bbd008ea737fb0378fbcefb5068ded0a74
   baseUrl = 'http://139.196.194.172:3000'
 } else {
   baseUrl = 'http://localhost:3000'
